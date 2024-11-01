719930ba149321dc2212434ca130dc688d60a5cc
Supakorn <supakorn.seet@bumail.net>
2024-11-01 23:11:23 +0700

    Halloween Event 0.2

--- Assets/Scripts/Event/HalloweenSetup.cs
+++ Assets/Scripts/Event/HalloweenSetup.cs
@@ -7,11 +7,27 @@ public class HalloweenSetup : MonoBehaviour
     [Header("Normal")]
     public GameObject normalPickUp;
     public GameObject halloweenPickUp;
+    public GameObject normalPlayer;
+    public GameObject halloweenPlayer;
+    public GameObject normalMap1;
+    public GameObject halloweenMap1;
+    public GameObject normalMap2;
+    public GameObject halloweenMap2;
+    public GameObject normalBG;
+    public GameObject halloweenBG;
 
     public void SetupHalloweenTheme(bool isActive)
     {
+        halloweenPlayer.SetActive(isActive);
+        normalPlayer.SetActive(!isActive);
         halloweenPickUp.SetActive(isActive);
         normalPickUp.SetActive(!isActive);
+        halloweenMap1.SetActive(isActive);
+        normalMap1.SetActive(!isActive);
+        halloweenMap2.SetActive(isActive);
+        normalMap2.SetActive(!isActive);
+        halloweenBG.SetActive(isActive);
+        normalBG.SetActive(!isActive);
     }
 
 }
